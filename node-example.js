6b8247d61f375d6ec862ce37c7f888f5b320997f
kyle roux <kyle@level2designs.com>
2015-03-03 07:53:37 -0800

    inital commit

---
+++ node-example.js
@@ -0,0 +1,32 @@
+/* nodular.js
+ *  examples of getting data from angular environment:
+ *
+ *      get $timeout service:
+ *        var $timeout = ng_load('$timeout');
+ *      get $interval service:
+ *        var $interval = ng_load('$interval');
+ *      get custom service:
+ *        var serv = ng_load('testService',['testApp']); <---additional modules refrenced are cached for later use
+ *      get more custom services without refrenceing the app 
+ *      like above, all ng_load calls are cached so any 
+ *      modules only need to be loaded once.
+ *        var fact = ng_load('testFactory');
+ *      get the $controller service to load controllers
+ *        var $c = ng_load('$controller');
+ */
+module.exports = function(){
+    console.log(angular.version);
+    /* load your modules by name here */
+    //var app = angular.module('testApp',['service.app','dice.factory.app','controllers-ng.app']);
+
+    /* require module files here */
+    //require('./service.js');
+    //require('./factorys.js');
+    //require('./controllers-ng.js');
+    //require('./newdice.js');
+
+    //var rootScope = ng_load('$rootScope');
+    //var $interval = ng_load('$interval');
+    //var $timeout = ng_load('$timeout');
+};
+
