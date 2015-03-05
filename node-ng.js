e819e7fdd6611fe1fe4138898617d50cd484b859
kyle roux <kyle@level2designs.com>
2015-03-04 16:57:55 -0800

    added promisify helper function

--- node-ng.js
+++ node-ng.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var get_mod = function(name){
     return require('/root/.node/lib/node_modules/'+name);
 };
@@ -24,6 +25,21 @@ var use_angular = function(callback){
                 }
                 cache_mods(mods);
                 return angular.injector(mods).get(name);
+            },
+            promisify:function(asyncFn,context){         
+                var $q = ng_load('$q');                
+                return function(){
+                    var defer = $q.defer(),
+                    args = Array.prototype.slice.call(arguments);        
+                    args.push(function(err,val){
+                        if(err !== null){
+                            return defer.reject(err);
+                        }
+                        return defer.resolve(val);
+                    });
+                    asyncFn.apply(context || {}, args);        
+                    return defer.promise;
+                };
             }
         });
         return callback();
